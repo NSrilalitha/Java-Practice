e0deeb694cb0c25460d1ecf42add2eb3982d1fb5
Srilalitha Nittala <srilalitha.nittala@gmail.com>
2025-07-15 12:23:09 +0530

    updated program with peek() demo

--- src/com/java8/features/practice/FilterEvenNumbers.java
+++ src/com/java8/features/practice/FilterEvenNumbers.java
@@ -12,11 +12,18 @@ public class FilterEvenNumbers {
         List<Integer> nums = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
         // print the even numbers
         nums.stream().filter(num -> num%2 == 0).forEach(num -> System.out.print(" "+num)); // using lambda expression
-        nums.stream().filter(num -> num%2 == 0).forEach(System.out::println); // using method reference
         // Collect the even numbers
         List<Integer> evenNums = nums.stream().filter(num -> num%2 == 0).collect(Collectors.toList());
         System.out.println("printing the even numbers list");
-        evenNums.forEach(System.out::println);
+        evenNums.forEach(System.out::println); // using method reference
+
+        // Demonstrate peek() to observe intermediate operations
+        List<Integer> evenNumbers = nums.stream()
+                .peek(n -> System.out.println("Original: " + n))
+                .filter(num -> num % 2 == 0)
+                .peek(n -> System.out.println("Filtered Even: " + n))
+                .collect(Collectors.toList());
+        evenNumbers.forEach(System.out::println);
 
     }
 }
